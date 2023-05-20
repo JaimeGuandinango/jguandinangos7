8232c0995354747cc690c8abb5d9bc2e2a80af6c
Jaime Guandinango <jamesguandis2103@hotmail.es>
2023-05-19 19:56:34 -0500

    cambios

--- jguandinangos7/Vistas/ConsultaRegistros.xaml.cs
+++ jguandinangos7/Vistas/ConsultaRegistros.xaml.cs
@@ -6,15 +6,48 @@ using System.Threading.Tasks;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using SQLite;
+using System.Runtime.CompilerServices;
+using System.Collections.ObjectModel;
 
 namespace jguandinangos7.Vistas
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class ConsultaRegistros : ContentPage
     {
+        private SQLiteAsyncConnection conexion;
+        private ObservableCollection<Estudiante> tablaEstudiante;
         public ConsultaRegistros()
         {
             InitializeComponent();
+            conexion = DependencyService.Get<Database>().GetConnection();
+            NavigationPage.SetHasBackButton(this, false);
+            ObtenerEstudiante();
+        }
+
+        public async void ObtenerEstudiante()
+        {
+            var ResulEstudiantes = await conexion.Table<Estudiante>().ToListAsync();
+            tablaEstudiante= new ObservableCollection<Estudiante>(ResulEstudiantes);
+            ListaEstudiantes.ItemsSource = tablaEstudiante;
+
+        }
+
+        private void ListaEstudiantes_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            var objetoEstudiante= (Estudiante)e.SelectedItem;
+            var ItemId= objetoEstudiante.Id.ToString();
+            int id = Convert.ToInt32(ItemId); //id
+            string nombre= objetoEstudiante.Nombre.ToString();
+            string usuario = objetoEstudiante.Usuario.ToString();
+            string contraseña= objetoEstudiante.Contraseña.ToString();
+            Navigation.PushAsync(new Elemento(id, nombre, usuario, contraseña));
+
+        }
+
+        private void btnSalir_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new Login());
         }
     }
 }
--- jguandinangos7/Vistas/Elemento.xaml.cs
+++ jguandinangos7/Vistas/Elemento.xaml.cs
@@ -3,18 +3,72 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using SQLite;
+using System.IO;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+
 
 namespace jguandinangos7.Vistas
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class Elemento : ContentPage
     {
-        public Elemento()
+        private int idSeleccionado;
+        private SQLiteAsyncConnection conexion;
+        IEnumerable<Estudiante> RActualizar;
+        IEnumerable<Estudiante> REliminar;
+        public Elemento(int id, string nombre, string usuario, string contraseña)
         {
             InitializeComponent();
+            txtId.Text= id.ToString();
+            txtNombre.Text= nombre;
+            txtUsuario.Text= usuario;
+            txtContraseña.Text = contraseña;
+            conexion = DependencyService.Get<Database>().GetConnection();
+            idSeleccionado = id;
+        }
+        public static IEnumerable<Estudiante> Eliminar(SQLiteConnection db, int id)
+        {
+            return db.Query<Estudiante>("DELETE FROM Estudiante where id =?", id);
+        }
+        public static IEnumerable<Estudiante> Actualizar(SQLiteConnection db, string nombre, string usuario, string contraseña, int id)
+        {
+            return db.Query<Estudiante>("UPDATE Estudiante set Nombre =?, Usuario=?, Contraseña=? where id=?", nombre, usuario, contraseña, id);
+        }
+        private void btnActualizar_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                var ruta = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "uisrael.db3");
+                var db= new SQLiteConnection(ruta);
+                RActualizar = Actualizar(db, txtNombre.Text, txtUsuario.Text, txtContraseña.Text, idSeleccionado);
+                Navigation.PushAsync(new ConsultaRegistros());
+
+            }
+            catch (Exception ex)
+            {
+                DisplayAlert("ALERTA", ex.Message, "Cerrar");
+
+
+            }
+        }
+
+        private void btnEliminar_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                var ruta = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "uisrael.db3");
+                var db = new SQLiteConnection(ruta);
+                REliminar = Eliminar(db, idSeleccionado);
+                Navigation.PushAsync(new ConsultaRegistros());
+
+            }
+            catch (Exception ex)
+            {
+                DisplayAlert("ALERTA", ex.Message, "Cerrar");
+ 
+            }
         }
     }
 }
